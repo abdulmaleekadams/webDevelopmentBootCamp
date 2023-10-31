e98d510def0b956e7cf1d8b6b94bb8b46bf11b30
abdulmaleekadams <harbdulmaleek.com>
2023-10-31 13:07:34 +0100

    Update Functionality

--- capstoneProject/index.js
+++ capstoneProject/index.js
@@ -50,8 +50,19 @@ async function getBooksRead() {
     console.error(err);
   }
 }
+async function getBooksReadWithoutImage() {
+  try {
+    const results = await client.query('SELECT * FROM books;');
+    let books = results.rows;
+
+    return books;
+  } catch (err) {
+    console.error(err);
+  }
+}
 
 async function getBooksReadByFilter(filterOption) {
+  filterOption = filterOption.split(' ').join('_').toLowerCase();
   try {
     if (['title', 'rating', 'date_read'].includes(filterOption)) {
       const query =
@@ -116,6 +127,55 @@ app.get('/sort', async (req, res) => {
 
   res.render('index.ejs', { booksRead: booksRead });
 });
+
+app.get('/admin', async (req, res) => {
+  const books = await getBooksReadWithoutImage();
+  res.render('admin.ejs', { books: books });
+});
+
+app.post('/update/:id', async (req, res) => {
+  const id = req.params.id;
+  const updatedBookDetails = req.body;
+  await updateBook(updatedBookDetails, id);
+  res.redirect('/admin');
+});
+
+app.post('/delete/:id', async (req, res) => {
+  const id = req.params.id;
+  await deleteBook(id);
+  res.redirect('/admin');
+});
+
+async function updateBook(updatedBookDetails, id) {
+  const {
+    title,
+    isbn,
+    author,
+    date_read,
+    rating,
+    description,
+    book_source,
+    book_source_url,
+  } = updatedBookDetails;
+  const results = await client.query(
+    'UPDATE books SET title = $1, isbn = $2, author = $3, date_read = $4, rating = $5, description = $6, book_source = $7, book_source_url = $8 WHERE id = $9',
+    [
+      title,
+      isbn,
+      author,
+      date_read,
+      rating,
+      description,
+      book_source,
+      book_source_url,
+      id,
+    ]
+  );
+}
+async function deleteBook(id) {
+  await client.query('DELETE FROM notes WHERE book_id = $1', [id]);
+  await client.query('DELETE FROM books WHERE id = $1', [id]);
+}
 
 app.listen(3000, () => {
   console.log(`Server running on http://localhost:3000`);
--- capstoneProject/public/script/main.js
+++ capstoneProject/public/script/main.js
@@ -18,8 +18,46 @@ const animateSection = () => {
   });
 };
 
+function autoGrowTextArea(element, textarea) {
+  textarea.value = element.innerText;
+  element.classList.remove('truncate');
+  element.style.height = element.scrollHeight + 'px';
+}
+
+const editBtns = document.querySelectorAll('.edit');
+
+if (editBtns) {
+  for (let editBtn of editBtns) {
+    const id = editBtn.id.split('-')[1];
+    const editForm = document.querySelector(`#editForm-${id}`);
+    const closeBtn = document.querySelector(`#closeBtn-${id}`);
+    const textArea = document.querySelector(`#form-${id}  .textarea`);
+    const textarea = document.querySelector(`#form-${id}  textarea`);
+    textarea.value = textArea.innerHTML;
+
+    const initialContent = textArea.innerHTML;
+
+    textArea.addEventListener('click', () =>
+      autoGrowTextArea(textArea, textarea)
+    );
+    textArea.addEventListener('input', () =>
+      autoGrowTextArea(textArea, textarea)
+    );
+    editBtn.addEventListener('click', () => editForm.classList.add('showUI'));
+    closeBtn.addEventListener('click', () =>
+      editForm.classList.remove('showUI')
+    );
+    closeBtn.addEventListener('click', () => {
+      textArea.innerText = initialContent;
+      textArea.classList.add('truncate');
+      textArea.style.height = '100px';
+    });
+  }
+}
+
 if (filterButton) {
   filterButton.addEventListener('click', handleFilter);
 }
+
 window.addEventListener('load', animateSection);
 window.addEventListener('scroll', animateSection);
